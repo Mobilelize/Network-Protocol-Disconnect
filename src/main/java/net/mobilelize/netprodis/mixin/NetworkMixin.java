010af568fd9394ddc6751cf28bdaf7ae1b7434b0
Mobilelize <mobilelize6@gmail.com>
2024-09-14 02:29:35 -0400

    Initial commit

---
+++ src/main/java/net/mobilelize/netprodis/mixin/NetworkMixin.java
@@ -0,0 +1,27 @@
+package net.mobilelize.netprodis.mixin;
+
+import net.minecraft.client.network.ClientCommonNetworkHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin({ClientCommonNetworkHandler.class})
+public class NetworkMixin {
+    private static final Logger LOGGER = LogManager.getLogger("netprodis");
+
+    public NetworkMixin() {
+    }
+
+    @Inject(
+            method = {"onPacketException"},
+            at = {@At("HEAD")},
+            cancellable = true
+    )
+    private void onPacketException(CallbackInfo ci) {
+        LOGGER.warn("Strict error handling was triggered, but disconnection was prevented");
+        ci.cancel();
+    }
+}
